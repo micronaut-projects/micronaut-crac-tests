da1238c1db2a47f0a370f97dfbe3b9eba154f6c1
Tim Yates <tim.yates@gmail.com>
2022-08-09 18:00:16 +0100

    Utils is one folder up

--- buildSrc/src/main/resources/script-preamble.sh
+++ buildSrc/src/main/resources/script-preamble.sh
@@ -10,11 +10,11 @@ profile() {
       -s "Startup completed" \
       java -jar $JAR)
   curl localhost:8080/hello/test | grep "Hello test!"
-  utils/bench.sh http://localhost:8080/hello/test
+  ../utils/bench.sh http://localhost:8080/hello/test
   kill $PROCESS
 
   echo "Prepare checkpoint"
-  PROCESS=$(utils/start-bg.sh \
+  PROCESS=$(../utils/start-bg.sh \
       -s "Startup completed" \
       -e exitcode \
       ${{ env.JDK }}/bin/java \
@@ -25,18 +25,18 @@ profile() {
       -jar $JAR)
 
   echo "Warmup"
-  utils/bench.sh http://localhost:8080/hello/test
+  ../utils/bench.sh http://localhost:8080/hello/test
   jcmd $PROCESS JDK.checkpoint
-  [ 137 = $(utils/read-exitcode.sh exitcode) ]
+  [ 137 = $(../utils/read-exitcode.sh exitcode) ]
 
   echo "Test restore"
-  PROCESS=$(utils/start-bg.sh \
+  PROCESS=$(../utils/start-bg.sh \
       -s "restore-finish" \
       ${{ env.JDK }}/bin/java -XX:CRaCRestoreFrom=cr)
   curl localhost:8080/hello/test | grep "Hello test!"
-  utils/bench.sh http://localhost:8080/hello/test
+  ../utils/bench.sh http://localhost:8080/hello/test
   kill $PROCESS
 
   echo "Check startup time"
-  timeout 3 bash -c "utils/javatime ; ${{ env.JDK }}/bin/java -XX:CRaCRestoreFrom=cr" | utils/sel.awk -v from=prestart -v to=restore-finish
+  timeout 3 bash -c "../utils/javatime ; ${{ env.JDK }}/bin/java -XX:CRaCRestoreFrom=cr" | ../utils/sel.awk -v from=prestart -v to=restore-finish
 }
