961b67901db4fda45bed8664390e0eba9e302f55
Tim Yates <tim.yates@gmail.com>
2022-08-15 16:43:16 +0100

    Limit CRaC feature to Java 17 (#6)

--- buildSrc/src/main/java/io/micronaut/crac/feature/CracFeatureValidator.java
+++ buildSrc/src/main/java/io/micronaut/crac/feature/CracFeatureValidator.java
@@ -4,6 +4,7 @@ import io.micronaut.starter.application.ApplicationType;
 import io.micronaut.starter.feature.Feature;
 import io.micronaut.starter.feature.graalvm.GraalVM;
 import io.micronaut.starter.feature.validation.FeatureValidator;
+import io.micronaut.starter.options.JdkVersion;
 import io.micronaut.starter.options.Options;
 import jakarta.inject.Singleton;
 
@@ -18,6 +19,9 @@ public class CracFeatureValidator implements FeatureValidator {
 
     @Override
     public void validatePostProcessing(Options options, ApplicationType applicationType, Set<Feature> features) {
+        if (options.getJavaVersion().majorVersion() < JdkVersion.JDK_17.majorVersion()) {
+            throw new IllegalArgumentException("CRaC needs at least JDK 17");
+        }
         if (features.stream().anyMatch(CracFeature.class::isInstance)
             && features.stream().anyMatch(GraalVM.class::isInstance)) {
             throw new IllegalArgumentException("CRaC and GraalVM cannot be combined");
