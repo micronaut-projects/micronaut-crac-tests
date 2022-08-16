c1562ab9e931721f6595bbd4774bb855314684c8
Tim Yates <tim.yates@gmail.com>
2022-08-16 12:36:59 +0100

    better scripts

--- buildSrc/src/main/groovy/io/micronaut/crac/CracProjectGenerator.groovy
+++ buildSrc/src/main/groovy/io/micronaut/crac/CracProjectGenerator.groovy
@@ -184,7 +184,9 @@ class CracProjectGenerator implements AutoCloseable {
 
     private static CracMetadata mergeMetadatas(CracMetadata base, CracMetadata metadata) {
         CracMetadata merged = new CracMetadata()
+        merged.slug = metadata.slug
         merged.base = metadata.base
+        merged.skip = metadata.skip
         merged.buildTools = metadata.buildTools ?: base.buildTools
         merged.languages = metadata.languages ?: base.languages
         merged.testFramework = metadata.testFramework ?: base.testFramework
