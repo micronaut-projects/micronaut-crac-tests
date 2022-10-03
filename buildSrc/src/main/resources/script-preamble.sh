cf0f6bc6605b3029c2f154a42aeeb445b2784016
Tim Yates <tim.yates@gmail.com>
2022-10-03 11:36:09 +0100

    Show speedup

--- buildSrc/src/main/resources/script-preamble.sh
+++ buildSrc/src/main/resources/script-preamble.sh
@@ -128,11 +128,11 @@ gradle() {
 
   echo "### Summary" >> $GITHUB_STEP_SUMMARY
   echo "" >> $GITHUB_STEP_SUMMARY
-  echo "| Build type | Time to First Request (secs) |" >> $GITHUB_STEP_SUMMARY
-  echo "| --- | --- |" >> $GITHUB_STEP_SUMMARY
-  echo "| Standard Docker | $standard |" >> $GITHUB_STEP_SUMMARY
-  echo "| Native Docker | $native |" >> $GITHUB_STEP_SUMMARY
-  echo "| CRaC Docker | $crac |" >> $GITHUB_STEP_SUMMARY
+  echo "| Build type | Time to First Request (secs) | Scale |" >> $GITHUB_STEP_SUMMARY
+  echo "| --- | --- | --- |" >> $GITHUB_STEP_SUMMARY
+  echo "| Standard Docker | $standard | $(bc -l <<< "scale=3; $standard/$standard") ($(bc -l <<< "scale=1; $standard/$standard")x) |" >> $GITHUB_STEP_SUMMARY
+  echo "| Native Docker | $native | $(bc -l <<< "scale=3; $native/$standard")  ($(bc -l <<< "scale=1; $standard/$native")x) |" >> $GITHUB_STEP_SUMMARY
+  echo "| CRaC Docker | $crac | $(bc -l <<< "scale=3; $crac/$standard")  ($(bc -l <<< "scale=1; $standard/$crac")x) |" >> $GITHUB_STEP_SUMMARY
   echo "" >> $GITHUB_STEP_SUMMARY
 
   assemble_gradle
