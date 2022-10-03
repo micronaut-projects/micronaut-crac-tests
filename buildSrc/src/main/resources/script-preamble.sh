9a155c9be89bc735d2572dcba648e96816c13e20
Tim Yates <tim.yates@gmail.com>
2022-10-03 10:43:24 +0100

    Add non-docker test back in

--- buildSrc/src/main/resources/script-preamble.sh
+++ buildSrc/src/main/resources/script-preamble.sh
@@ -4,6 +4,69 @@ FAILED_PROJECTS=()
 EXIT_STATUS=0
 DELAY=30
 
+echo "=== Utils located at '$UTILS'"
+echo "=== CRaC JDK located at '$JDK'"
+
+read_exit_code() {
+  local exitcode=$1
+  local retries=60 # 30 seconds of 0.5 second sleeps
+  e=$(head $exitcode 2>/dev/null)
+  while [ ! $e ] && [ $retries -gt 0 ]; do
+    sleep 0.5
+    e=$(head $exitcode 2>/dev/null)
+    retries=$((retries-1))
+  done
+  rm $exitcode
+  if [ $retries -le 0 ]; then
+    echo "Timeout waiting for $1"
+    return 1
+  else
+    echo $e
+  fi
+}
+
+test_checkpoint() {
+  local JAR=$1
+  echo "Vanilla test"
+  local PROCESS=$($UTILS/start-bg.sh \
+      -s "Startup completed" \
+      java -jar $JAR)
+
+  EXPECTED_RESPONSE='Hello test!'
+  RESPONSE=$(curl -s localhost:8080/hello/test)
+  if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+  kill $PROCESS
+
+  echo "Prepare checkpoint"
+  PROCESS=$($UTILS/start-bg.sh \
+      -s "Startup completed" \
+      -e exitcode \
+      $JDK/bin/java \
+      -XX:CRaCCheckpointTo=cr \
+      -XX:+UnlockDiagnosticVMOptions \
+      -XX:+CRTraceStartupTime \
+      -Djdk.crac.trace-startup-time=true \
+      -jar $JAR)
+  echo "Make checkpoint"
+  jcmd $PROCESS JDK.checkpoint
+  local foundExitCode="$(readexitcode exitcode)"
+  if [ "137" != "$foundExitCode" ]; then
+    echo "ERROR: Expected exit code 137, got $foundExitCode"
+    kill $PROCESS
+    return 1
+  else
+    echo "Test restore"
+    PROCESS=$($UTILS/start-bg.sh \
+        -s "restore-finish" \
+        $JDK/bin/java -XX:CRaCRestoreFrom=cr)
+    RESPONSE=$(curl -s localhost:8080/hello/test)
+    if [ "$RESPONSE" != "$EXPECTED_RESPONSE" ]; then echo $RESPONSE && exit 1; fi
+    kill $PROCESS
+    echo "Remove Checkpoint"
+    rm -rf cr
+  fi
+}
+
 execute() {
   # We only want to wait  seconds for success
   local end=$((SECONDS+DELAY))
@@ -43,6 +106,10 @@ build_gradle_docker_crac() {
   ./gradlew dockerBuildCrac || EXIT_STATUS=$?
 }
 
+assemble_gradle() {
+  ./gradlew assemble
+}
+
 gradle() {
   # Build regular app in docker, and rename image to micronautguide-standard
   build_gradle_docker
@@ -61,10 +128,14 @@ gradle() {
 
   echo "### Summary" >> $GITHUB_STEP_SUMMARY
   echo "" >> $GITHUB_STEP_SUMMARY
-  echo "| Build type | TTFR |" >> $GITHUB_STEP_SUMMARY
+  echo "| Build type | Time to First Request (secs) |" >> $GITHUB_STEP_SUMMARY
   echo "| --- | --- |" >> $GITHUB_STEP_SUMMARY
-  echo "| Standard | $standard |" >> $GITHUB_STEP_SUMMARY
-  echo "| Native | $native |" >> $GITHUB_STEP_SUMMARY
-  echo "| CRaC | $crac |" >> $GITHUB_STEP_SUMMARY
+  echo "| Standard Docker | $standard |" >> $GITHUB_STEP_SUMMARY
+  echo "| Native Docker | $native |" >> $GITHUB_STEP_SUMMARY
+  echo "| CRaC Docker | $crac |" >> $GITHUB_STEP_SUMMARY
   echo "" >> $GITHUB_STEP_SUMMARY
+
+  assemble_gradle
+  test_checkpoint 'build/libs/micronautguide-0.1-all.jar' || EXIT_STATUS=$?
+  echo "test_checkpoint exit code: \$EXIT_STATUS" >> $GITHUB_STEP_SUMMARY
 }
