95a82d8e745834eb5573ba6f2a687bfd3632eb85
Tim Yates <tim.yates@gmail.com>
2022-10-03 09:44:59 +0100

    Github action summary

--- buildSrc/src/main/resources/script-preamble.sh
+++ buildSrc/src/main/resources/script-preamble.sh
@@ -16,17 +16,24 @@ execute() {
   done
 }
 
-foo=$(TIMEFORMAT="%U"; time sleep 2)
+# From http://mywiki.wooledge.org/BashFAQ/032 capture the elapsed time in seconds
+mytime() {
+  exec 3>&1 4>&2
+  mytime=$(TIMEFORMAT="%R"; { time $1 1>&3 2>&4; } 2>&1)
+  exec 3>&- 4>&-
+  echo $mytime
+}
 
-time_to_first_request() {
+time_to_first_request_docker() {
   CONTAINER=$(docker run -d -p 8080:8080 --privileged $1)
   echo ""
   echo "======================================="
   echo "TIME TO FIRST SUCCESSFUL REQUEST FOR $1"
-  time execute || EXIT_STATUS=$?
+  result=$(mytime execute)
   echo "======================================="
   echo ""
   docker kill $CONTAINER
+  echo $result
 }
 
 build_gradle_docker() {
@@ -42,15 +49,26 @@ build_gradle_docker_crac() {
 }
 
 gradle() {
+  # Build regular app in docker, and rename image to micronautguide-standard
   build_gradle_docker
   docker tag micronautguide:latest micronautguide-standard:latest
+
+  # Build native app in docker, and rename image to micronautguide-native
   build_gradle_docker_native
   docker tag micronautguide:latest micronautguide-native:latest
+
+  # Build crac app in docker
   build_gradle_docker_crac
-  time_to_first_request micronautguide-standard:latest
-  time_to_first_request micronautguide-native:latest
-  time_to_first_request micronautguide:latest
-  time_to_first_request micronautguide-standard:latest
-  time_to_first_request micronautguide-native:latest
-  time_to_first_request micronautguide:latest
+
+  standard=$(time_to_first_request_docker micronautguide-standard:latest)
+  native=$(time_to_first_request_docker micronautguide-native:latest)
+  crac=$(time_to_first_request_docker micronautguide:latest)
+
+  echo "### Summary" >> $GITHUB_STEP_SUMMARY
+  echo "" >> $GITHUB_STEP_SUMMARY
+  echo "| Build type | TTFR |" >> $GITHUB_STEP_SUMMARY
+  echo "| --- | --- |" >> $GITHUB_STEP_SUMMARY
+  echo "| Standard | $standard |" >> $GITHUB_STEP_SUMMARY
+  echo "| Native | $native |" >> $GITHUB_STEP_SUMMARY
+  echo "| CRaC | $crac |" >> $GITHUB_STEP_SUMMARY
 }
