5297f317e2fe94885fb40ba70807c7ee48d8eb21
Tim Yates <tim.yates@gmail.com>
2023-09-13 15:28:35 +0100

    Fix CRaC JDK download now there's also a JRE

--- cracDownload.sh
+++ cracDownload.sh
@@ -5,7 +5,7 @@ CRAC_ARCH=$(uname -m)
 
 echo "Downloading CRaC OpenJDK $CRAC_JDK_VERSION for $CRAC_ARCH"
 
-release_id=$(curl -s "https://api.azul.com/metadata/v1/zulu/packages/?java_version=${CRAC_JDK_VERSION}&arch=${CRAC_ARCH}&crac_supported=true&latest=true&release_status=ga&certifications=tck&page=1&page_size=100" -H "accept: application/json" | jq -r '.[0] | .package_uuid')
+release_id=$(curl -s "https://api.azul.com/metadata/v1/zulu/packages/?java_version=${CRAC_JDK_VERSION}&arch=${CRAC_ARCH}&crac_supported=true&java_package_type=jdk&latest=true&release_status=ga&certifications=tck&page=1&page_size=100" -H "accept: application/json" | jq -r '.[0] | .package_uuid')
 if [ "$release_id" = "null" ]; then
   echo "No CRaC OpenJDK $CRAC_JDK_VERSION for $CRAC_ARCH found"
   exit 1
